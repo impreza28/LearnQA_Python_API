8dba44fda1d5edf0b46039bd41f1534a1d0d8ddc
Валерия Русакова <rusakovalera@gmail.com>
2023-05-03 14:05:48 +0300

    Ex10: Тест на короткую фразу

--- Ex10.py
+++ Ex10.py
@@ -2,5 +2,5 @@ class TestEx10():
     def test_phrase(self):
         phrase = len(list(input("Set a phrase: "))) # посчитать длину листа из фразы
         lenPhrase = 15 #граница по условию
-        assert phrase < 15, f"Фраза больше {lenPhrase} символов"
+        assert phrase < lenPhrase, f"Фраза больше {lenPhrase} символов"
 
