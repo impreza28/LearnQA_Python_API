84d9c6c29b158bc9ec20832c7e2997f009d86917
Валерия Русакова <rusakovalera@gmail.com>
2023-05-12 19:50:32 +0300

    Ex18: Тесты на DELETE

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -2,6 +2,7 @@ import requests
 import pytest
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 
 class TestUserAuth(BaseCase):
@@ -13,7 +14,7 @@ class TestUserAuth(BaseCase):
         data = {'email': 'vinkotov@example.com',
                 'password': '1234'
                 }
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        response1 = MyRequests.post("/user/login", data=data)
 
         self.auth_sid = self.get_cookie(response1,"auth_sid")
         self.token = self.get_header(response1,"x-csrf-token")
@@ -21,10 +22,7 @@ class TestUserAuth(BaseCase):
 
     def test_auth_user(self):
 
-        response2 = requests.get(
-            "https://playground.learnqa.ru/api/user/auth",
-            headers={"x-csrf-token": self.token},
-            cookies={"auth_sid": self.auth_sid})
+        response2 = MyRequests.get("/user/auth", headers={"x-csrf-token": self.token}, cookies={"auth_sid": self.auth_sid})
 
         Assertions.assert_json_value_by_name(
             response2, "user_id",
@@ -36,14 +34,9 @@ class TestUserAuth(BaseCase):
     def test_negative_auth_user(self, condition):
 
         if condition == "no_cookie":
-            response2 = requests.get(
-                "https://playground.learnqa.ru/api/user/auth",
-                headers={"x-csrf-token": self.token})
+            response2 = MyRequests.get("/user/auth", headers={"x-csrf-token": self.token})
         else:
-            response2 = requests.get(
-                "https://playground.learnqa.ru/api/user/auth",
-                headers={"auth_sid": self.auth_sid})
+            response2 = MyRequests.get("/user/aut", headers={"auth_sid": self.auth_sid})
 
         Assertions.assert_json_value_by_name(
-            response2, "user_id", 0, f"User is authorize with condition {condition}"
-        )
+            response2, "user_id", 0, f"User is authorize with condition {condition}")
