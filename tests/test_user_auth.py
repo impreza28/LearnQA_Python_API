1e214105fdc5ee93724b8540f303729c13bbe88f
Валерия Русакова <rusakovalera@gmail.com>
2023-05-14 19:48:51 +0300

    Allure и первоначальная настройка

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,10 +1,11 @@
 import requests
 import pytest
+import allure
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
 
-
+@allure.epic("Authorization cases")
 class TestUserAuth(BaseCase):
     exclude_params = [
         ("no_cookie"),
@@ -19,7 +20,7 @@ class TestUserAuth(BaseCase):
         self.auth_sid = self.get_cookie(response1,"auth_sid")
         self.token = self.get_header(response1,"x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
-
+    @allure.description("Тест авторизации по емейлу и паролю")
     def test_auth_user(self):
 
         response2 = MyRequests.get("/user/auth", headers={"x-csrf-token": self.token}, cookies={"auth_sid": self.auth_sid})
@@ -29,7 +30,7 @@ class TestUserAuth(BaseCase):
             self.user_id_from_auth_method, "User id from auth method is not equal to user id from check mathod"
         )
 
-
+    @allure.description("Тест проверяет статус авторизации без отправки куки или хэдера")
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negative_auth_user(self, condition):
 
